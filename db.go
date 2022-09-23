bd27f7a11c9b7c045d632b059bad62561710ce92
Плохотнюк Артем <aplokhotnyuk@diasoft.ru>
2022-09-23 13:22:01 +0300

    init

---
+++ db.go
@@ -0,0 +1,105 @@
+package db
+
+import (
+	"github.com/Nixson/db/postgres"
+	"github.com/Nixson/environment"
+	"github.com/Nixson/logNx"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"math"
+)
+
+var gormInstance *gorm.DB
+
+func DbInit(env *environment.Env) {
+	switch env.GetString("db.driver") {
+	case "postgres":
+		postgres.DbInit(env)
+		gormInstance = postgres.Get()
+	}
+}
+
+func Get() *gorm.DB {
+	if gormInstance == nil {
+		panic("not init DB")
+	}
+	return gormInstance
+}
+
+type LoggerWriter struct {
+	LogLevel logger.LogLevel
+}
+
+func (w *LoggerWriter) Write(p []byte) (n int, err error) {
+	switch w.LogLevel {
+	case logger.Silent:
+		break
+	case logger.Error:
+		logNx.GetLogger().Error(string(p))
+	case logger.Warn:
+		logNx.GetLogger().Warning(string(p))
+	case logger.Info:
+		logNx.GetLogger().Info(string(p))
+	}
+	return len(p), nil
+}
+
+type Pagination struct {
+	Page       int
+	Size       int
+	TotalRows  int64
+	TotalPages int64
+}
+
+type PaginationResponse struct {
+	Content  []interface{} `json:"content"`
+	Pageable struct {
+		Sort struct {
+			Sorted   bool `json:"sorted"`
+			Unsorted bool `json:"unsorted"`
+			Empty    bool `json:"empty"`
+		} `json:"sort"`
+		PageNumber int  `json:"pageNumber"`
+		PageSize   int  `json:"pageSize"`
+		Offset     int  `json:"offset"`
+		Paged      bool `json:"paged"`
+		Unpaged    bool `json:"unpaged"`
+	} `json:"pageable"`
+	TotalPages    int64 `json:"totalPages"`
+	TotalElements int64 `json:"totalElements"`
+	Last          bool  `json:"last"`
+	Sort          struct {
+		Sorted   bool `json:"sorted"`
+		Unsorted bool `json:"unsorted"`
+		Empty    bool `json:"empty"`
+	} `json:"sort"`
+	NumberOfElements int  `json:"numberOfElements"`
+	First            bool `json:"first"`
+	Size             int  `json:"size"`
+	Number           int  `json:"number"`
+	Empty            bool `json:"empty"`
+}
+
+func Paginate(page, size int, p *Pagination, t interface{}) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+
+		switch {
+		case size > 100:
+			size = 100
+		case size <= 0:
+			size = 10
+		}
+
+		offset := (page - 1) * size
+
+		Get().Model(t).Count(&p.TotalRows)
+		p.TotalPages = int64(math.Ceil(float64(p.TotalRows) / float64(size)))
+		p.Page = page
+		p.Size = size
+
+		return db.Offset(offset).Limit(size)
+	}
+}
---
+++ postgres/postgres.go
@@ -0,0 +1,55 @@
+package postgres
+
+import (
+	"fmt"
+	"github.com/Nixson/db"
+	"github.com/Nixson/environment"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"time"
+)
+
+var gormInstance *gorm.DB
+
+func Get() *gorm.DB {
+	if gormInstance == nil {
+		panic("not init DB")
+	}
+	return gormInstance
+}
+
+func DbInit(env *environment.Env) {
+	logLevel := logger.Silent
+	if env.GetBool("db.showSql") {
+		logLevel = logger.Info
+	}
+	newLogger := logger.New(
+		log.New(&db.LoggerWriter{LogLevel: logLevel}, "", 0), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logLevel,    // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,       // Disable color
+		},
+	)
+	db, err := gorm.Open(
+		postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d search_path=%s sslmode=%s",
+			env.GetString("db.host"),
+			env.GetString("db.login"),
+			env.GetString("db.password"),
+			env.GetString("db.name"),
+			env.GetInt("db.port"),
+			env.GetString("db.schema"),
+			env.GetString("db.ssl")),
+		),
+		&gorm.Config{
+			Logger: newLogger,
+		})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	gormInstance = db
+}
